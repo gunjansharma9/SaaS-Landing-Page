a7db6acb95511f36cb2e555e3d1b1888538cb540
gunjansharma9 <gunjansharmahapur@gmail.com>
2024-09-22 16:14:58 +0530

    Initial Commit

---
+++ src/sections/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <a>
+                <img src="/images/xora.svg" width={115} height={55} alt="" />
+            </a>
+        </div>
+    </header>
+  )
+}
+
+export default Header
